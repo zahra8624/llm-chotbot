d02a6766f6629b589dc0a63777a7ccd9115c3232
zahrareisi <sania.8624@gmail.com>
2026-05-12 16:13:16 +0330

    initializing

---
+++ 2-system-prompt.py
@@ -0,0 +1,68 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+from openai import OpenAI
+
+def add_user_message(messages, content):
+    messages.append({"role":"user", "content":content})
+
+def add_assistant_message(messages, content):
+    messages.append({"role":"assistant", "content":content}) 
+
+def chat(messages):
+    client= OpenAI(base_url="https://api.gapgpt.app/v1")
+    response=client.chat.completions.create(
+    
+        model="gpt-5-nano",
+        max_tokens=3000,
+        messages=messages
+    )
+    # print("*************************************************")
+    # print(response.choices)
+    # print("*************************************************")
+    return response.choices[0].message.content
+
+def display_messages(messages):
+    """Display all messages begin to sent to the LLM"""
+    print("\n" + "=" *60)
+    print("MESSAGES BEGIN SENT TO LLM:")
+    print("=" *60)
+    for i, msg in enumerate(messages,1):
+        role= msg["role"].upper()
+        content=msg["content"]
+        print(f"\n[Message {i} - {role}]")
+        print(f"{content}")
+    print("=" * 60 + "\n")    
+
+def main():
+    system_prompt = """
+You are an expert system design mentor specializing in software architecture and distributed systems.
+Do not answer directly user's questions. Instead, guide them through the process of designing system by asking probing questions, providing hints and encouraging critical thinking.
+gide them through the solution step by step.  
+"""
+    messages=[
+        {"role":"system","content":system_prompt}
+    ]
+
+    print("Simple console chatbot")
+    print("Type 'quit', 'exit' or 'bye' to end the conversation.\n ")
+
+    while True:
+        #get user input
+        user_input=input("You: ").strip()
+        if user_input.lower() in ["quit","exit", "buy"]:
+            print("\nGoodBuy!")
+            break
+        if not user_input:
+            continue
+        add_user_message(messages,user_input)
+        display_messages(messages)
+
+        print("Thinking...\n")
+        response=chat(messages)
+        print(f"Assistant: {response} \n")
+        add_assistant_message(messages,response)
+    
+
+if __name__ == "__main__":
+    main()
